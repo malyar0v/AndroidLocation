337af5cd1086830e3875f3236ab2a40fb7f203c1
Maksim <mmalia@ttu.ee>
2020-03-28 00:19:41 +0200

    Main activity UI, notification layout, navigation, controls

--- app/src/main/java/ee/taltech/mmalia/MapsActivity.kt
+++ app/src/main/java/ee/taltech/mmalia/MapsActivity.kt
@@ -9,6 +9,7 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -16,7 +17,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_maps)
+        setContentView(R.layout.activity_main)
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
